fe91a0b852fa518ae6b6011fbdeb2d02e2d87e64
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-08-17 14:15:48 +0800

    Add conStr placeholder in DataHelper.cs

--- DataAccess/DataHelper.cs
+++ DataAccess/DataHelper.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Runtime.InteropServices;
 
 namespace DataAccess
 {
     public class DataHelper
     {
+        string connectionString = ConfigurationManager.ConnectionStrings["MasterDb"].ConnectionString;
+
+
     }
 }
